bab346c822a7b689acaceb8113765eab41c1bf26
kurves <kavssimply@gmail.com>
2024-10-07 21:41:38 +0300

    update app settings

--- jengaHub/urls.py
+++ jengaHub/urls.py
@@ -16,16 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-from jengaHubApp import views
 
-router = DefaultRouter()
-router.register(r'projects', views.ProjectViewSet)
-router.register(r'feedbacks', views.FeedbackViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('jengahub/', include('jengaHubApp.urls', namespace='jengaHubApp')),
 
 ]
 
