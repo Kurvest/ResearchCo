e918d9898c634f580ec11a2da4c29ab559d5214b
kurves <kavssimply@gmail.com>
2024-10-15 13:34:08 +0300

    configure static files

--- jengaHub/urls.py
+++ jengaHub/urls.py
@@ -32,4 +32,5 @@ path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
