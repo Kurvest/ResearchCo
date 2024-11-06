8b11cc362c1bdb424844dce187582748406e2f04
kurves <kavssimply@gmail.com>
2024-11-06 22:55:56 +0300

    update models

--- jengaHubApp/models.py
+++ jengaHubApp/models.py
@@ -42,3 +42,25 @@ class News(models.Model):
 
     def __str__(self):
         return self.title
+
+class Advertisement(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='advertisements/', blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class RentalListing(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='rentals/', blank=True, null=True)
+    location = models.CharField(max_length=255)
+    monthly_rent = models.DecimalField(max_digits=10, decimal_places=2)
+    available_from = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
